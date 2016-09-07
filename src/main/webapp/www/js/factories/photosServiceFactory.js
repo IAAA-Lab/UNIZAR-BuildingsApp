270e5102cc5e1f6ba3ac806ce86c58acfba8c755
daniegarcia254 <daniegarcia254@gmail.com>
2016-09-08 00:31:47 +0200

    Photo service factory with function to get room photo's paths

---
+++ src/main/webapp/www/js/factories/photosServiceFactory.js
@@ -0,0 +1,34 @@
+/**********************************************************************
+ * infoService: Servicio que define todas las llamadas al web service para recoger los datos
+ ***********************************************************************/
+
+UZCampusWebMapApp.factory('photosService', function($http, $q, $timeout, $state, $rootScope, APP_CONSTANTS) {
+
+        var count = function (roomId) {
+            var deferred = $q.defer();
+            var request = {
+                method: 'GET',
+                url: APP_CONSTANTS.URI_API + 'photos/'+roomId+'/',
+                contentType: 'application/json',
+                dataType: "json"
+            };
+            console.log(request);
+            $timeout(function () {
+                $http(request).then(
+                    function (result) {
+                        deferred.resolve(result.data);
+                    },
+                    function(err){
+                        console.log("Error count photos: ",err);
+                        deferred.reject(err);
+                    }
+                );
+            });
+            return deferred.promise;
+        };
+
+        //Definición de las funciones anteriores para poder ser utilizadas
+        return {
+            count: count
+        };
+    });
